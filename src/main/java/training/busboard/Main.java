ca39bcf7c3e7fe842a93bfba812cb3e0eb5cbf47
Ben Davies <b_j_d@hotmail.co.uk>
2018-09-24 10:40:24 +0100

    added stuff

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,25 @@
 package training.busboard;
 
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        
     }
+    
+    public static void createClient() {
+    	Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+    	List<Widget> widgets = client
+    		    .path("/widgets")
+    		    .request(MediaType.APPLICATION_JSON_TYPE)
+    		    .get(new GenericType<List<Widget>() {});
+    }
+    
 }	
